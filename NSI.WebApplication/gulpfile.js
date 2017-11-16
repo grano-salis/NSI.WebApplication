38b2912cc0b06171d8d50b48bb4a2838aec5def5
Dino Alic <alic.dino@gmail.com>
2017-11-16 12:53:37 +0100

    Adjust gulp file to copy app and node modules.

--- NSI.WebApplication/gulpfile.js
+++ NSI.WebApplication/gulpfile.js
@@ -7,15 +7,25 @@
     browserSync = require('browser-sync').create();
     cache = require('gulp-cached'); //If cached version identical to current file then it doesn't pass it downstream so this file won't be copied 
 
-gulp.task('default', 'copy-node_modules');
-
 gulp.task('copy-node_modules', function () {
 
     try {
-
         gulp.src('node_modules/**')
             .pipe(cache('node_modules'))
             .pipe(gulp.dest('wwwroot/node_modules'));
+    }
+    catch (e) {
+        return -1;
+    }
+    return 0;
+});
+
+gulp.task('copy-angularapp', function () {
+
+    try {
+        gulp.src('app/**')
+            .pipe(cache('app'))
+            .pipe(gulp.dest('wwwroot/app'));
     }
     catch (e) {
         return -1;
@@ -72,3 +82,5 @@ gulp.task('watch', function () {
     // Watch .scss files
     gulp.watch('src/scss/*.scss', ['sass', 'sass-minify']);
 });
+
+gulp.task('default', ['copy-node_modules', 'copy-angularapp', 'browser-sync', 'sass-minify', 'sass', 'scripts', 'watch']);
