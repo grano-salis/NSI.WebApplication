59410e494533d93ca48820476f9fbf9957336ebe
Vejsil Hrustic <vhrustic1@etf.unsa.ba>
2017-11-25 23:16:46 +0100

    Contacts Rest controller with getContacts method

---
+++ NSI.BLL/Interfaces/IContactsManipulation.cs
@@ -0,0 +1,12 @@
+﻿using NSI.DC.TaskRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSI.BLL.Interfaces
+{
+    public interface IContactsManipulation
+    {
+        ICollection<DC.ContactsRepository.ContactDto> GetContacts();
+    }
+}
