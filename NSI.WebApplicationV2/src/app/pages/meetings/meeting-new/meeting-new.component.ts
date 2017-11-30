4b0d2be6c9c7118c30b3ffd22408c0d16aa06315
Amar Basic <amarbasic4@gmail.com>
2017-11-30 11:54:48 +0100

    new meeting form setup

--- NSI.WebApplicationV2/src/app/pages/meetings/meeting-new/meeting-new.component.ts
+++ NSI.WebApplicationV2/src/app/pages/meetings/meeting-new/meeting-new.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Meeting } from './meeting';
 
 @Component({
   selector: 'app-meeting-new',
   templateUrl: './meeting-new.component.html',
   styleUrls: ['./meeting-new.component.scss']
 })
-export class MeetingNewComponent implements OnInit {
+export class MeetingNewComponent {
 
-  constructor() { }
+  users = ["user 1", "user 2", "user 3", "user 5", "user 4"]
 
-  ngOnInit() {
+  query: string;
+  filteredList: string[];
+  model: Meeting;
+
+  constructor() {
+    this.query = '';
+    this.filteredList = [];
+    this.model = new Meeting();
+  }
+
+  filter() {
+    if (this.query.length > 2) {
+      this.filteredList = this.users.filter(function (el: string) {
+        return el.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+      }.bind(this));
+    } else {
+      this.filteredList = [];
+    }
+  }
+
+  add(item:string) {
+    this.model.users.push(item);
+    this.query = '';
+    this.filteredList = [];
+  }
+
+  remove(item:string){
+    this.model.users.splice(this.model.users.indexOf(item),1);
+}
+
+  onSubmit() {
+    console.log("Form submitted");
+    console.log(this.model);
+  }
+
+  newMeeting() {
+    this.model = new Meeting();
   }
 
 }
---
+++ NSI.WebApplicationV2/src/app/pages/meetings/meeting-new/meeting.ts
@@ -0,0 +1,11 @@
+export class Meeting {
+
+    title: string;
+    date: string;
+    users: string[];
+
+    constructor() {
+        this.users = [];
+    }
+
+}
