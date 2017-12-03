fe1028e2fa93ce14c27794e998261799a1ff3849
irmakarasoftic <irma.karasoftic@gmail.com>
2017-12-04 00:22:14 +0100

    added method to get all documents by page

--- NSI.DC/DocumentRepository/DocumentDto.cs
+++ NSI.DC/DocumentRepository/DocumentDto.cs
@@ -10,5 +10,17 @@ namespace NSI.DC.DocumentRepository
     {
         [DataMember]
         public int DocumentId {get; set;}
+        public int FileTypeExtension { get; set; }
+        public string DocumentTitle { get; set; }
+        public int CaseId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryId { get; set; }
+
+        public string DocumentDescription { get; set; }
+        public string DocumentPath { get; set; }
+        public string DocumentContent { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModified { get; set; }
     }
 }
