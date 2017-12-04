e1708592c5c6877a4090e0391a473db010d7f2fd
imandzankovic <idzankovic1@etf.unsa.ba>
2017-12-04 13:44:14 +0100

    POST method in CaseInfoController

--- NSI.REST/Controllers/CaseInfoController.cs
+++ NSI.REST/Controllers/CaseInfoController.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NSI.BLL.Interfaces;
+using NSI.REST.Models;
+using NSI.Repository;
+using NSI.DC.CaseRepository;
+using NSI.BLL.Interfaces;
+using NSI.Repository;
+using System.Net.Http;
+using System.Net;
+using NSI.REST.Models;
+using NSI.REST.Middleware;
+using NSI.Repository.Mappers;
 
 namespace NSI.REST.Controllers
 {
@@ -11,34 +21,65 @@ namespace NSI.REST.Controllers
     [Route("api/CaseInfo")]
     public class CaseInfoController : Controller
     {
-		ICaseInfoManipulation _caseInfoManipulation { get; set; }
+		ICaseInfoManipulation _caseInfoRepository { get; set; }
 
 		public CaseInfoController(ICaseInfoManipulation caseInfoManipulation)
 		{
-			_caseInfoManipulation = caseInfoManipulation;
+			_caseInfoRepository = caseInfoManipulation;
 		}
 
 		// GET: api/CaseInfo
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok(_caseInfoManipulation.GetCasesInfo());
+			return Ok(_caseInfoRepository.GetCasesInfo());
 		}
 
-		//      // GET: api/CaseInfo/5
-		//[HttpGet("{id}", Name = "Get")]
-		//public IActionResult Get(int id)
-		//{
-		//	//return Ok(_caseInfoManipulation.GetCaseInfoById(id));
-		//	return Ok();
-		//}
-
-		// POST: api/CaseInfo
-		[HttpPost]
-        public void Post([FromBody]string value)
+        //      // GET: api/CaseInfo/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
+            //return Ok(_caseInfoRepository.GetCaseInfoById(id));
+            return Ok();
         }
-        
+
+        // POST: api/CaseInfo
+        [HttpPost]
+        public IActionResult Post([FromBody]CaseInfoCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            CaseInfoDto caseInfoDto = new CaseInfoDto()
+            {
+                CaseNumber = model.CaseNumber,
+                CourtNumber = model.CourtNumber,
+                Value = model.Value,
+                Judge = model.Judge,
+                Court = model.Court,
+                CounterParty=model.CounterParty,
+                Note = model.Note,
+                 CaseCategory=model.CaseCategory,
+                CustomerId=model.CustomerId,
+                ClientId=model.ClientId,
+                CreatedByUserId=model.CreatedByUserId
+            };
+
+            try
+            {
+                var caseInfo = _caseInfoRepository.CreateCaseInfo(caseInfoDto);
+                if (caseInfo != null)
+                    return Ok(caseInfo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
+        }
+
         // PUT: api/CaseInfo/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
