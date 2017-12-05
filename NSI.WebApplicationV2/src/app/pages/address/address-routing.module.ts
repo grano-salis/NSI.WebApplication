cee657637aaa2a291be632a942c004f93e3adf5b
Mesud Klisura <mklisura1@etf.unsa.ba>
2017-12-05 22:01:41 +0100

    Address List, New component

--- NSI.WebApplicationV2/src/app/pages/address/address-routing.module.ts
+++ NSI.WebApplicationV2/src/app/pages/address/address-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddressComponent } from './address.component';
 import { extract } from '../../core/services/i18n.service';
+import {HearingNewComponent} from '../hearings/hearing-new/hearing-new.component';
+import {AddressNewComponent} from './address-new/address-new.component';
+import {AddressListComponent} from './address-list/address-list.component';
 
 const routes: Routes = [
-  { path: '', component: AddressComponent, data: { title: extract('Address') } }
+  { path: '', component: AddressComponent, data: { title: extract('Address') } },
+  { path: 'new', component: AddressNewComponent },
+  { path: 'list', component: AddressListComponent}
 ];
 
 @NgModule({
