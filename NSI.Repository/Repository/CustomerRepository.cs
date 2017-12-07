f0f060bad40b0ec9b9932437dd45c036b10e3337
sakib <sakib.kurtic@gmail.com>
2017-12-07 23:00:06 +0100

    CRUD

--- NSI.Repository/Repository/CustomerRepository.cs
+++ NSI.Repository/Repository/CustomerRepository.cs
@@ -1,10 +1,133 @@
-﻿using System;
-namespace NSI.Repository.Repository
+﻿using IkarusEntities;
+using NSI.DC.CustomersRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NSI.Repository.Interfaces;
+
+namespace NSI.Repository
 {
-    public class CustomerRepository
+    public partial class CustomerRepository : ICustomerRepository
     {
-        public CustomerRepository()
+        private readonly IkarusContext _dbContext;
+
+        public CustomerRepository(IkarusContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            try
+            {
+                var customer = Mappers.CustomerRepository.MapToDbEntity(customerDto);
+                _dbContext.Add(customer);
+                if (_dbContext.SaveChanges() != 0)
+                    return Mappers.CustomerRepository.MapToDto(customer);
+            }
+            catch (Exception ex)
+            {
+                //log ex
+                throw ex.InnerException;
+                //throw new Exception("Database error!");
+            }
+            return null;
+
+        }
+
+        public CustomerDto GetCustomerById(int customerId)
+        {
+            try
+            {
+                var customer = _dbContext.Customer.FirstOrDefault(x => x.CustomerId == customerId);
+                if (customer != null)
+                {
+                    return Mappers.CustomerRepository.MapToDto(customer);
+                }
+            }
+            catch (Exception ex)
+            {
+                //log ex
+                Console.WriteLine(ex.InnerException);
+                throw new Exception("Database error!");
+            }
+            return null;
+        }
+
+        public ICollection<CustomerDto> GetCustomers()
+        {
+            try
+            {
+                var customer = _dbContext.Customer;
+                if (customer != null)
+                {
+                    ICollection<CustomerDto> customerDto = new List<CustomerDto>();
+                    foreach (var item in customer)
+                    {
+                        customerDto.Add(Mappers.CustomerRepository.MapToDto(item));
+                    }
+                    return customerDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                //log ex
+                throw new Exception("Database error!");
+            }
+            return null;
+        }
+
+        public bool DeleteCustomerById(int customerId)
+        {
+            try
+            {
+                var customer = _dbContext.Customer.FirstOrDefault(x => x.CustomerId == customerId);
+                if (customer != null)
+                {
+                    customer.IsDeleted = true;
+                    _dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                //log ex
+                throw new Exception("Database error!");
+            }
+        }
+
+        public ICollection<CustomerDto> SearchCustomer(CustomerDto searchCriteria)
+        {
+            if (searchCriteria == null)
+            {
+                throw new ArgumentNullException("searchCriteria");
+            }
+
+
+            return null;
+
+        }
+
+        public bool EditCustomer(int customerId, CustomerDto customer)
+        {
+            try
+            {
+                var customerTmp = _dbContext.Customer.FirstOrDefault(x => x.CustomerId == customerId);
+                if (customerTmp != null)
+                {
+                    customerTmp.CustomerName = customer.CustomerName ?? customerTmp.CustomerName;
+                    _dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                //log ex
+                throw new Exception("Database error!");
+            }
         }
     }
 }
