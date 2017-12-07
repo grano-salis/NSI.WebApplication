09bbffadf409b1abe5d67d06b0b8c62826dc1796
esensar <es.ensar@gmail.com>
2017-12-07 20:57:13 +0100

    Add customers on frontend
     * Add simple customers service and display
     * Fix issues in customer list component
     * Add customer details
     * Connect customers with backend
     * Minor fix in delete link

--- NSI.WebApplicationV2/src/app/pages/organization/oraganization.module.ts
+++ NSI.WebApplicationV2/src/app/pages/organization/oraganization.module.ts
@@ -1,16 +1,20 @@
 import { CustomersDetailsComponent } from './CustomerDetails/CustomersDetailsComponent';
 import { CustomersListComponent } from './CustomersList/customersListComponent';
+import { CustomersService } from '../../services/customers.service';
 import { OrganizationRoutingModule } from './oraganization-routing.module';
 import { SharedModule } from './../../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
    imports: [
     CommonModule,
     SharedModule,
+		FormsModule,
     OrganizationRoutingModule
   ],
-  declarations: [CustomersListComponent, CustomersDetailsComponent]
+  declarations: [CustomersListComponent, CustomersDetailsComponent],
+	providers: [CustomersService]
 })
 export class OrganizationModule { }
