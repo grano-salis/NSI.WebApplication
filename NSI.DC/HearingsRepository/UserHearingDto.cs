8295f8d1051cb042b75822fedfd7b05e7ce75da0
Amar Basic <amarbasic4@gmail.com>
2017-12-10 16:51:36 +0100

    Data validation on backend added

--- NSI.DC/HearingsRepository/UserHearingDto.cs
+++ NSI.DC/HearingsRepository/UserHearingDto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace NSI.DC.HearingsRepository
 {
     public class UserHearingDto
     {
+        [DataMember]
         public int UserId { get; set; }
+
+        [DataMember]
         public string UserName { get; set; }
     }
 }
--- NSI.DC/MeetingsRepository/UserMeetingDto.cs
+++ NSI.DC/MeetingsRepository/UserMeetingDto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace NSI.DC.MeetingsRepository
 {
     public class UserMeetingDto
     {
+        [DataMember]
         public int UserId { get; set; }
+
+        [DataMember]
         public string UserName { get; set; }
     }
 }
