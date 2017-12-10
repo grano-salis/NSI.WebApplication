cffe27908bb382a6801b2d786236e511e2711c70
ncorovic1 <nino.corovic@gmail.com>
2017-12-10 20:06:49 +0100

    Redesigning and working on documents front-end.

---
+++ NSI.WebApplicationV2/src/app/pages/documents/models/mockDocumentDetails.ts
@@ -0,0 +1,43 @@
+import { DocumentDetails } from './index.model';
+
+export var MDD: DocumentDetails[] = [
+    {
+        documentId: 11, 
+        caseId: 1,
+        documentCategoryId: 1,
+        fileTypeId: 1,
+        documentPath: "C:",
+        documentContent: "",
+        createdByUserId: 1,
+        author: "Amir",
+        caseNumber: 12345678,
+        documentCategoryName: "Ime dokumenta",
+        fileIconPath: "fa-file-word-o"
+    },
+    {
+        documentId: 11, 
+        caseId: 1,
+        documentCategoryId: 1,
+        fileTypeId: 1,
+        documentPath: "C:",
+        documentContent: "",
+        createdByUserId: 1,
+        author: "Amir",
+        caseNumber: 12345678,
+        documentCategoryName: "Ime dokumenta",
+        fileIconPath: "fa-file-word-o"
+    },
+    {
+        documentId: 11, 
+        caseId: 1,
+        documentCategoryId: 1,
+        fileTypeId: 1,
+        documentPath: "C:",
+        documentContent: "",
+        createdByUserId: 1,
+        author: "Amir",
+        caseNumber: 12345678,
+        documentCategoryName: "Ime dokumenta",
+        fileIconPath: "fa-file-word-o"
+    }
+];
