67cb62c99e81a7de89545617f4c36ceac797ee8d
Haris Spahic <spahicharis@users.noreply.github.com>
2017-12-11 22:23:38 +0100

    [NSI-GOLF] MeetingsModule - Unos novog meetinga odabirom datuma
    Refactoring Meetings overview komponente
    Dodan AlertService (potrebno pokrenuti "npm i")

--- NSI.WebApplicationV2/src/app/pages/meetings/meetings-overview/meetings-overview.component.ts
+++ NSI.WebApplicationV2/src/app/pages/meetings/meetings-overview/meetings-overview.component.ts
@@ -5,6 +5,8 @@ import * as moment from 'moment';
 import {Logger} from '../../../core/services/logger.service';
 import {HelperService} from "../../../services/helper.service";
 import {MeetingsService} from "../../../services/meetings.service";
+import {AlertService} from "../../../services/alert.service";
+import {Router} from "@angular/router";
 
 declare let $: any;
 
@@ -46,7 +48,8 @@ export class MeetingsComponent implements OnInit {
   formSubmitted: boolean;
 
 
-  constructor(private meetingsService: MeetingsService) {
+  constructor(private meetingsService: MeetingsService, private alertService: AlertService,
+              private router: Router) {
 
     this.calendarConfiguration.select = (start: any, end: any) => this._onSelect(start, end);
   }
@@ -59,36 +62,6 @@ export class MeetingsComponent implements OnInit {
     this.loadMeetings();
   }
 
-  submitEvent(form: any) {
-
-    this.formSubmitted = true;
-    $(this._calendar).fullCalendar('unselect');
-    if (form.invalid) {
-      return;
-    }
-    let eventData = {
-      start: this.eventModel.dateFrom,
-      end: this.eventModel.dateTo,
-      title: this.eventModel.title
-    };
-
-    $(this._calendar).fullCalendar('renderEvent', eventData, true);
-    this.dateSelected = false;
-    this.formSubmitted = false;
-    $(this._calendar).fullCalendar('unselect');
-    //todo: add service
-  }
-
-  cancelNewEvent() {
-    this.eventModel = {
-      dateFrom: null,
-      dateTo: null,
-      title: '',
-      desc: ''
-    };
-    this.dateSelected = false;
-  }
-
   onCalendarReady(calendar: any): void {
     this._calendar = calendar;
     $(this._calendar).fullCalendar('option', 'contentHeight', 650);
@@ -98,28 +71,8 @@ export class MeetingsComponent implements OnInit {
   // Private methods
   // ****************
   private _onSelect(start: any, end: any): void {
-
     if (this._calendar != null) {
-
-      this.dateSelected = true;
-
-      HelperService.scrollToTop();
-
-      this.eventModel = {
-        dateFrom: start,
-        dateTo: end
-      };
-
-
-      // if (title) {
-      //   eventData = {
-      //     title: title,
-      //     start: start,
-      //     end: end
-      //   };
-      //   $(this._calendar).fullCalendar('renderEvent', eventData, true);
-      // }
-
+      this.router.navigate(['/meetings/new'], {queryParams: {dateFrom: start, dateTo: end}});
     }
   }
 
@@ -127,7 +80,8 @@ export class MeetingsComponent implements OnInit {
     this.meetingsService.getMeetings()
       .subscribe((r: any) => {
         const temp: any[] = [];
-        each(r, (item) => {
+        let meetings = r.data;
+        each(meetings, (item) => {
           temp.push({
             title: 'Meeting ID: ' + item.meetingId,
             start: item.from,
@@ -137,34 +91,9 @@ export class MeetingsComponent implements OnInit {
         });
 
         $(this._calendar).fullCalendar('renderEvents', temp, true);
+      }, e => {
+        this.alertService.showError(e.Message); //TODO: check???
+        console.log("error", e);
       });
   }
-
-  private getData() {
-    return [
-      {
-        title: 'All Day Event',
-        start: '2017-11-01',
-        color: '#223442'
-      },
-      {
-        title: 'Long Event',
-        start: '2017-11-07',
-        end: '2017-12-10',
-        color: '#223442'
-      },
-      {
-        title: 'Dinner',
-        start: '2017-11-14T20:00:00',
-        color: '#223442'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2017-11-01T07:00:00',
-        color: '#223442'
-      }
-    ];
-  }
-
-
 }
