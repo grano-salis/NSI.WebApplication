ea298b7a07d06c1244afbb607944652816302aaf
Irma Karasoftic <irmaka@maestralsolutions.com>
2017-12-12 00:52:18 +0100

    worked on API

--- NSI.BLL/Interfaces/IDocumentManipulation.cs
+++ NSI.BLL/Interfaces/IDocumentManipulation.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using NSI.DC.Common;
-using NSI.DC.DocumentRepository;
+﻿using NSI.DC.DocumentRepository;
 using NSI.REST.Models;
 
 namespace NSI.BLL.Interfaces
@@ -8,9 +6,7 @@ namespace NSI.BLL.Interfaces
     public interface IDocumentManipulation
     {
         DocumentDto GetDocumentById(int documentId);
-        ICollection GetCaseDocuments(int caseId);
-        ICollection GetCustomerDocuments(int customerId, Paging paging);
-        PagingResultModel<DocumentDto> GetDocumentsByPage(REST.Models.DocumentsPagingQueryModel query);
+        PagingResultModel<DocumentDto> GetDocumentsByPage(DocumentsPagingQueryModel query);
         DocumentDto SaveDocument();
         bool DeleteDocument(int id);
         bool EditDocument(int id, DocumentDto documentDto);
