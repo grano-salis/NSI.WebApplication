b4bbd775ab43a4e36fda9fe13416cbc03ed92c6e
dzenana1 <dkapetanov1@etf.unsa.ba>
2017-12-13 20:49:49 +0100

    edit case fix

--- NSI.WebApplicationV2/src/app/pages/cases/edit-case/edit-case.component.ts
+++ NSI.WebApplicationV2/src/app/pages/cases/edit-case/edit-case.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CasesService} from '../../../services/cases.service';
-import {Case} from "../new-case/case";
+import {Case} from '../new-case/case';
 
 @Component({
   selector: 'app-edit-case',
@@ -25,23 +25,16 @@ export class EditCaseComponent implements OnInit {
 
   loadCase(): any {
     this.caseService.getCaseById(this.caseToEditId).subscribe( data => {
-      console.log('dataedit', data)
+      console.log('dataedit', data);
       this.model = data;
     });
   }
 
   onSubmit() {
     console.log('this.model', this.model);
-    // this.model.dateCreated = new Date().toLocaleDateString();
-    // this.model.dateModified = null;
-    // this.model.caseCategory = 1;
-    // this.model.customerId = 1;
-    // this.model.clientId = 1;
-    // this.model.createdByUserId = 1;
-    // console.log('this.model',this.model)
-    // this.casesService.postCase(this.model).subscribe(data =>{
-    //   console.log('data',data);
-    //   this.router.navigate(['cases/all']);
-    // });
+    this.caseService.putCase(this.model.caseId, this.model).subscribe(data => {
+      console.log('data', data);
+      this.router.navigate(['cases/all']);
+    });
   }
 }
