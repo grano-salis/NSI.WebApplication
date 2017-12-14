7f952048c13b23df5228243ee54306bcef150973
Irma Karasoftic <irmaka@maestralsolutions.com>
2017-12-14 13:08:26 +0100

    get works

--- NSI.DC/DocumentRepository/DocumentDto.cs
+++ NSI.DC/DocumentRepository/DocumentDto.cs
@@ -8,17 +8,27 @@ namespace NSI.DC.DocumentRepository
     {
         [DataMember]
         public int DocumentId {get; set;}
+        [DataMember]
         public int FileTypeExtension { get; set; }
+        [DataMember]
         public string DocumentTitle { get; set; }
+        [DataMember]
         public int CaseId { get; set; }
+        [DataMember]
         public string CategoryName { get; set; }
+        [DataMember]
         public int CategoryId { get; set; }
 
+        [DataMember]
         public string DocumentDescription { get; set; }
+        [DataMember]
         public string DocumentPath { get; set; }
+        [DataMember]
         public string DocumentContent { get; set; }
 
+        [DataMember]
         public DateTime CreatedAt { get; set; }
+        [DataMember]
         public DateTime LastModified { get; set; }
     }
 }
