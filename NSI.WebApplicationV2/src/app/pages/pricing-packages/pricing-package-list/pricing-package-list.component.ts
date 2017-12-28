88700558c3ae668e9daf0bd24e8eb08c1e1500fd
AmarPanjeta <apanjeta1@etf.unsa.ba>
2017-12-28 16:20:52 +0100

    info modal added

--- NSI.WebApplicationV2/src/app/pages/pricing-packages/pricing-package-list/pricing-package-list.component.ts
+++ NSI.WebApplicationV2/src/app/pages/pricing-packages/pricing-package-list/pricing-package-list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PricingPackagesService } from '../../../services/pricing-packages.service';
+import { SubscriptionService } from '../../../services/subscription.service';
+import {ModalModule} from "ngx-modal";
+
+
+
 @Component({
   selector: 'app-pricing-package-list',
   templateUrl: './pricing-package-list.component.html',
@@ -7,18 +12,30 @@ import { PricingPackagesService } from '../../../services/pricing-packages.servi
 })
 export class PricingPackageListComponent implements OnInit {
 
+  @ViewChild('myModal') modal: any;
   pricingPackages:any[]=[];
+  name:string = "names";
+  bonus:number = 0;
 
-
-  constructor(private pricingPackagesService: PricingPackagesService) { }
+  constructor(private pricingPackagesService: PricingPackagesService, private subscriptionService: SubscriptionService) { }
 
   ngOnInit() {
     this.loadPricingPackages();
+    //this.modal.open();
   }
 
   private loadPricingPackages(): void {
     this.pricingPackagesService.getPricingPackages().
     subscribe(pricingPackages => this.pricingPackages = pricingPackages);
+  }
+
+  closeModal(){
+    console.log("log log log log log log log ")
+  }
+
+  subscribeToPackage(num:number){
+    console.log(num);
+    this.modal.open();
   }
 
 }
