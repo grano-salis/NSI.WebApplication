fe9913a5a4c6b99383122983cdb6abeda5ff4f13
MerisaGolic <mgolic1@etf.unsa.ba>
2018-01-05 21:49:23 +0100

    Unit tests update, meetings and hearings

--- NSI.Tests/MeetingsControllerTest.cs
+++ NSI.Tests/MeetingsControllerTest.cs
@@ -7,6 +7,7 @@ using NSI.DC.MeetingsRepository;
 using NSI.Repository;
 using NSI.Repository.Interfaces;
 using NSI.REST.Controllers;
+using NSI.REST.Models;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -128,15 +129,55 @@ namespace NSI.Tests
             Assert.IsType<OkObjectResult>(result);
         }
 
-        IkarusContext db = new IkarusContext();
-        IMeetingsRepository imr => new MeetingsRepository(db);
-        IMeetingsManipulation imm => new MeetingsManipulation(imr);
+        [Fact]
+        public void Update_ThrowsAnException()
+        {
+            // Arrange
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+
+            // Act
+            var result = controller.Put(-1, null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+
+        }
+
         [Fact]
         public void GetAllMeetings_ReturnsOK()
         {
-            // Arrange & Act
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
 
-            var controller = new MeetingsController(imm);
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
 
             // Act
             var result = controller.GetAll();
@@ -148,10 +189,14 @@ namespace NSI.Tests
         [Fact]
         public void GetMeetingById_ReturnsNoContent()
         {
-            var controller = new MeetingsController(imm);
+            // Arrange
+            var meetingRepo = new Mock<IMeetingsRepository>();
+
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
 
             // Act
-            var result = controller.Get(1);
+            var result = controller.Get(-1);
 
             // Assert
             Assert.IsType<BadRequestObjectResult>(result);
@@ -161,10 +206,38 @@ namespace NSI.Tests
         [Fact]
         public void GetMeetingById_ReturnsOK()
         {
-            var controller = new MeetingsController(imm);
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
 
             // Act
-            var result = controller.Get(21);
+            var result = controller.Get(123);
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
@@ -225,6 +298,332 @@ namespace NSI.Tests
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void Delete_ThrowsAnException()
+        {
+            // Arrange
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+
+            // Act
+            var result = controller.DeleteMeeting(-1);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+
+        }
+
+        [Fact]
+        public void GetMeetingByUserId_ReturnsOK()
+        {
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetingsByUser(1);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+
+        [Fact]
+        public void GetMeetingByUserId_ReturnsBadRequest()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetingsByUser(-11);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+        [Fact]
+        public void GetMeetingTimes_ReturnsOK()
+        {
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    },
+                    new UserMeetingDto()
+                    {
+                        UserId = 2
+                    },
+                    new UserMeetingDto()
+                    {
+                        UserId = 3
+                    },
+                    new UserMeetingDto()
+                    {
+                        UserId = 4
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetingsTimes(new List<int> { 1, 2, 3, 4 }, from, to, 10, 123);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetMeetingTimes_UserIdReturnsBadRequest()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetingsTimes(new List<int> { -1, 2, 3, 4 }, DateTime.Now, DateTime.Now, 10, 10);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetMeetingTimes_MeetingIdReturnsBadRequest()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetingsTimes(new List<int> { 1, 2, 3, 4 }, DateTime.Now, DateTime.Now, 10, -1);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void CheckUsersAvailability_ReturnsOK()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.CheckUsersAvailability(new List<int> { 1, 2, 3, 4 }, DateTime.Now, DateTime.Now, 1);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void CheckUsersAvailability_UserIdReturnsBadRequest()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.CheckUsersAvailability(new List<int> { -1, 2, 3, 4 }, DateTime.Now, DateTime.Now, 10);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void CheckUsersAvailability_MeetingIdReturnsBadRequest()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.CheckUsersAvailability(new List<int> { 1, 2, 3, 4 }, DateTime.Now, DateTime.Now, -1);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+        [Fact]
+        public void Search_ThrowsException()
+        {
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingsSearchModel()
+            {
+                MeetingId = id,
+                Title = title,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            var result = controller.Search(meeting, 1, 1);
+            // Assert
+            Assert.IsType<ObjectResult>(result);
+        }
+        [Fact]
+        public void Search_ReturnsOK()
+        {
+            //ToBeImplemented
+        }
+        [Fact]
+        public void Search_ThrowsNSIException()
+        {
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            var result = controller.Search(null, 1, 1);
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+
+        }
+
+        [Fact]
+        public void GetMeetings_ReturnsOK()
+        {
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetings(null, null);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void GetMeetings_ThrowsException()
+        {
+            // Arrange
+            int id = 123;
+            string title = "naziv";
+            string meetPlace = "mjesto";
+            DateTime from = DateTime.Now;
+            DateTime to = DateTime.Now.AddDays(10);
+
+            var usersOnMeeting = new List<UserMeetingDto>()
+                {
+                    new UserMeetingDto()
+                    {
+                        UserId = 1
+                    }
+                };
+
+            var meeting = new MeetingDto()
+            {
+                MeetingId = id,
+                Title = title,
+                MeetingPlace = meetPlace,
+                From = from,
+                To = to,
+                UserMeeting = usersOnMeeting
+            };
+
+            var meetingRepo = new Mock<IMeetingsRepository>();
+            meetingRepo.Setup(x => x.InsertMeeting(meeting));
+            var meetingManipulation = new MeetingsManipulation(meetingRepo.Object);
+            var controller = new MeetingsController(meetingManipulation);
+
+            // Act
+            var result = controller.GetMeetings(1, 1);
+
+            // Assert
+            Assert.IsType<ObjectResult>(result);
+        }
+        [Fact]
+        public void GetMeetings_ThrowsNSIException()
+        {
+            //ToBeImplemented
         }
     }
 }
