2c218f9acc1b10a7581b179a4e7370afdf0ae812
Admira <ahusic5@etf.unsa.ba>
2018-01-08 21:58:12 +0100

    Moved validation to BLL

--- NSI.BLL/ContactsManipulation.cs
+++ NSI.BLL/ContactsManipulation.cs
@@ -3,7 +3,15 @@ using NSI.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSI.DC.ContactsRepository;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Text.RegularExpressions;
+using NSI.DC.AddressRepository;
 namespace NSI.BLL
 {
     public class ContactsManipulation : Interfaces.IContactsManipulation
@@ -48,6 +56,29 @@ namespace NSI.BLL
             return _contactsRepository.EditContactById(contactId, contact);
         }
 
-     
+        public string ValidationContact(ContactDto contact)
+        {
+            String validationMessage = "";
+
+            if (string.IsNullOrEmpty(contact.FirsttName)) validationMessage += " First name is required.";
+            if (string.IsNullOrEmpty(contact.LastName)) validationMessage += " Last name is required.";
+            if (contact.Phones.Any(x => String.IsNullOrEmpty(x.PhoneNumber))) validationMessage += " All phone number fields should have a value.";
+            if (contact.Phones.Select(x => x.PhoneNumber).Distinct().Count() != contact.Phones.Count) validationMessage += " Phone number already exists or the same phone number value is entered several times.";
+            if (contact.Emails.Any(x => String.IsNullOrEmpty(x.EmailAddress))) validationMessage += " All email fields should have a value.";
+            if (contact.Emails.Select(x => x.EmailAddress).Distinct().Count() != contact.Emails.Count) validationMessage += " Email already exists or the same email address is enetered several times.";
+            if (!contact.Emails.All(x => Regex.IsMatch(x.EmailAddress, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))) validationMessage += "\n Email is not in correct format. Example: someone@domain.com.";
+            if (!Regex.IsMatch(contact.FirsttName, @"^[a-zA-Z]+$")) validationMessage += " First name should contain letters only.";
+            if (!Regex.IsMatch(contact.LastName, @"^[a-zA-Z]+$")) validationMessage += " Last name should contain letters only.";
+
+            foreach (string p in contact.Phones.Select(x => x.PhoneNumber))
+            {
+                Int64 result;
+                if (!Int64.TryParse(p, out result)) validationMessage += " Phone number " + p + " should contain numbers only.";
+            }
+
+            return validationMessage;
+        }
+
+
     }
 }
